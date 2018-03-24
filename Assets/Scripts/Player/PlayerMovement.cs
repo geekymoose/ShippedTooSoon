eb98e422c8f1fd557ff21da34bbef9ee2d03c9d6
Frederic Levesque <flevesque124@gmail.com>
2018-03-24 12:43:02 -0400

    Update movement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,84 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-
-
-    private Vector2 movementVector;
-    
-    [Range(0,100)]
+public class PlayerMovement : MonoBehaviour
+{
+    [Range(0, 100)]
     public float speed = 5f;
-    public float distanceToPickup = 1f;
-    public float throwForce = 1f;
 
-    Rigidbody2D body2d;
-    RaycastHit2D hit;
-    private bool isGrab = false;
-    public Transform pickupObject;
-    public LayerMask no;
+    private Rigidbody2D body2d;
+    private bool canPickup = false;
+    private Transform objectToPickUp;
+    private Vector2 movementVector;
+    private Vector2 forward;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    private void Start()
+    {
         body2d = GetComponent<Rigidbody2D>();
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+
+        objectToPickUp = GameObject.FindGameObjectWithTag("Pickup").transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //the player have to be in the collider of the pickup object if he want to pick up the object
         if (Input.GetButtonDown("A"))
         {
-            if (!isGrab)
+            if (canPickup)
             {
-                Physics2D.queriesStartInColliders = false;
-                hit = Physics2D.Raycast(transform.position, movementVector, distanceToPickup);
-                if(hit.collider!=null && hit.collider.tag == "Pickup")
-                {
-                    isGrab = true;
-                }
+                objectToPickUp.transform.parent = transform;
                 
+                objectToPickUp.gameObject.GetComponent<Rigidbody2D>().simulated = false;
             }
-            else if (!Physics2D.OverlapPoint(pickupObject.position, no))
-            {
-                isGrab = false;
-
-                if (hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
-                {
-                    //hit.collider.gameObject.GetComponent<Rigidbody2D>(). = new Vector2(transform.localScale.x, 1) * throwForce;
-                }
-            }
-
-            if (isGrab)
-            {
-                hit.collider.gameObject.transform.position = pickupObject.position;
-            }
-            
         }
         
-        
-	}
+
+        //when the player release the button drop the object
+        if (Input.GetButtonUp("A"))
+        {
+            Drop();
+        }
+
+        //Vector2 vectorToTarget = transform.position - new Vector3(movementVector.x,movementVector.y);
+        //float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x);
+    }
 
     private void FixedUpdate()
     {
+        //movement with the axis of the xbox gamepad
         movementVector.x = Input.GetAxis("LeftJoystickX") * speed;
         movementVector.y = Input.GetAxis("LeftJoystickY") * speed;
-        
-       body2d.velocity = new Vector2(movementVector.x, movementVector.y);
+
+        //move the player
+        body2d.velocity = new Vector2(movementVector.x, movementVector.y);
+
+        //rotate the player forward the direction
+        float degrees = Mathf.Atan2(movementVector.y, movementVector.x) * Mathf.Rad2Deg;
+
+        //to make only rotate in 90 degrees
+        if (degrees % 90 == 0)
+        {
+            body2d.rotation = degrees;
+        }
     }
 
-    
-
+    //drop the object when the player release the button
     public void Drop()
     {
-        pickupObject.parent = null;
-        pickupObject.gameObject.AddComponent(typeof(Rigidbody2D));
-        pickupObject = null;
+        objectToPickUp.parent = null;
+        objectToPickUp = GameObject.FindGameObjectWithTag("Pickup").transform;
+        objectToPickUp.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+        objectToPickUp.gameObject.GetComponent<Rigidbody2D>().simulated = true;
+        canPickup = false;
     }
 
-    void OnDrawGizmos()
+    private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
 
-        Gizmos.DrawRay(transform.position, movementVector);
+        Gizmos.DrawRay(transform.position, movementVector.normalized);
+    }
+
+    //if the player collide on the object that he can pickup
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Pickup")
+        {
+            canPickup = true;
+        }
+
+        
     }
 }
