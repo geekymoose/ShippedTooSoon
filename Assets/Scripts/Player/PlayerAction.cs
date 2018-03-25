3be6aa47acd7fd5e77355d03ae562d6040765670
geekymoose <constantin.masson@umontreal.ca>
2018-03-25 13:11:30 -0400

    Pickable working (But ugly)

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 public class PlayerAction : MonoBehaviour {
-    private Transform pickObj = null;
-	private Collider2D pickupCollider = null;
+    private Transform currentPickedObj = null;
+	private CircleCollider2D pickupCollider = null;
 
 	public Transform playerHand = null;
 
@@ -22,55 +22,52 @@ public class PlayerAction : MonoBehaviour {
 
 	void Update () {
         if (Input.GetButtonDown("Fire1")) {
-			if(this.pickObj != null) {
-				Debug.Log("PlayerMovement::PICKUP");
-				this.pickObj.transform.parent = this.playerHand;
-				//this.pickObj.gameObject.GetComponent<Rigidbody2D>().simulated = false;
+			Debug.Log("PlayerMovement::PICKUP");
+			this.currentPickedObj = this.getFirstPickupInRange();
+			if(this.currentPickedObj != null) {
+				this.currentPickedObj.transform.parent = this.playerHand;
 			}
         }
-
         //when the player release the button drop the object
         else if (Input.GetButtonUp("Fire1")) {
-			if(this.pickObj != null) {
+			if(this.currentPickedObj != null) {
 				Debug.Log("PlayerMovement::DROP");
 				this.drop();
 			}
         }
 	}
-
-
-	// -------------------------------------------------------------------------
-	// GamePlay Methods
-	// -------------------------------------------------------------------------
-
-    //drop the object when the player release the button
-    public void drop() {
-        if(this.pickObj != null) {
-            Debug.Log("PlayerMovement::drop()");
-            this.pickObj.parent = null;
-            //this.pickObj.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            //this.pickObj.gameObject.GetComponent<Rigidbody2D>().simulated = true;
-        }
-    }
-
-    //if the player collide on the object that he can pickup
+	
     private void OnTriggerEnter2D(Collider2D other) {
-        if (other.gameObject.tag == "Pickup") {
-            this.pickObj = other.gameObject.transform;
-        }
-        else if(other.CompareTag("Goal")) {
+        if(other.CompareTag("Goal")) {
             RoomGoal roger = other.gameObject.GetComponent<RoomGoal>();
             Assert.IsNotNull(roger, "Goal object doesn't have a RoomGoal script");
             roger.activate();
         }
     }
 
-    //deactivate the pickup when the player exit the collider
-    private void OnTriggerExit2D(Collider2D other) {
-		if(this.pickObj != null && this.pickObj.name == other.gameObject.name) {
-			this.pickObj = null;
-		}
+
+	// -------------------------------------------------------------------------
+	// GamePlay Methods
+	// -------------------------------------------------------------------------
+
+    public void drop() {
+        if(this.currentPickedObj != null) {
+            Debug.Log("PlayerMovement::drop()");
+            this.currentPickedObj.parent = null;
+            //this.pickObj.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            //this.pickObj.gameObject.GetComponent<Rigidbody2D>().simulated = true;
+        }
     }
 
-
+	private Transform getFirstPickupInRange() {
+		Vector2 center = new Vector2(this.transform.position.x, this.transform.position.y);
+		Collider2D[] cocos = Physics2D.OverlapCircleAll(center, this.pickupCollider.radius/2);
+		
+		foreach(Collider2D coco in cocos) {
+			if(coco.CompareTag("Pickup") || coco.CompareTag("Destructable")) {
+				return coco.transform;
+			}
+		}
+		return null;
+	}
 }
