730282579711a993f2e43e70d5c6e139a83bd668
Pierre <ld.pierre.hayeur@gmail.com>
2018-03-25 13:29:17 -0400

    Add enemy and several modification on environment (Level can be done)

--- Assets/Scripts/Game/RoomDoor.cs
+++ Assets/Scripts/Game/RoomDoor.cs
@@ -4,26 +4,40 @@ using UnityEngine;
 using UnityEngine.Assertions;
 
 public class RoomDoor : MonoBehaviour {
-	
-	public void openDoor() {
+
+    public bool isOpen = true;
+
+    public void openDoor() {
+        this.isOpen = true;
+
 		SpriteRenderer soso = this.GetComponent<SpriteRenderer>();
 		BoxCollider2D coco = this.GetComponent<BoxCollider2D>();
+        Animator anim = this.GetComponent<Animator>();
 
-		Assert.IsNotNull(soso, "Unable to recover TheDoors renderer buddy!");
+        Assert.IsNotNull(soso, "Unable to recover TheDoors renderer buddy!");
 		Assert.IsNotNull(coco, "Unable to recover TheDoors collider buddy!");
+        Assert.IsNotNull(anim, "Unable to recover TheDoors animator buddy!");
 
-		soso.enabled = false;
+        //soso.enabled = false;
 		coco.enabled = false;
-	}
+
+        //anim.SetTrigger("openDoor");
+    }
 
 	public void closeDoor() {
+        this.isOpen = false;
+
 		SpriteRenderer soso = this.GetComponent<SpriteRenderer>();
 		BoxCollider2D coco = this.GetComponent<BoxCollider2D>();
+        Animator anim = this.GetComponent<Animator>();
 
-		Assert.IsNotNull(soso, "Unable to recover TheDoors renderer buddy!");
+        Assert.IsNotNull(soso, "Unable to recover TheDoors renderer buddy!");
 		Assert.IsNotNull(coco, "Unable to recover TheDoors collider buddy!");
+        Assert.IsNotNull(anim, "Unable to recover TheDoors animator buddy!");
 
-		soso.enabled = true;
+        //soso.enabled = true;
 		coco.enabled = true;
-	}
+        
+        //anim.SetTrigger("closeDoor");
+    }
 }
