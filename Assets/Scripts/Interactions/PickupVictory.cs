b7d287ac96045f07538ae09cb12a85c65ec6f5db
geekymoose <constantin.masson@umontreal.ca>
2018-03-24 22:25:07 -0400

    Update Victory conditions

--- Assets/Scripts/Interactions/PickupVictory.cs
+++ Assets/Scripts/Interactions/PickupVictory.cs
@@ -5,14 +5,25 @@ using UnityEngine.Assertions;
 
 [CreateAssetMenu(fileName = "NewPickupVictory", menuName = "pickupVictory", order = 1)]
 public class PickupVictory : VictoryCondition {
-	public GameObject goalObject;
+	public GameObject[] listGoals;
 
 	public override void initConditions() {
-		this.goalObject = GameObject.Find(goalObject.name);
-		Assert.IsNotNull(this.goalObject, "Unable to locate the GoalObject");
+		for(int k = 0; k < this.listGoals.Length; ++k) {
+			this.listGoals[k] = GameObject.Find(this.listGoals[k].name);
+			Assert.IsNotNull(this.listGoals[k], "Unable to locate the GoalObject");
+		}
 	}
 
-    public override bool isValidated() {
-		return this.goalObject == null; // Goal has been picked up (destroyed)
-    }
+	public override bool isValidated() {
+		if(this.listGoals == null || this.listGoals.Length == 0) {
+			return true;
+		}
+		int remaining = 0;
+		foreach(GameObject o in this.listGoals) {
+			if(o != null) {
+				remaining++;
+			}
+		}
+		return remaining == 0;
+	}
 }
