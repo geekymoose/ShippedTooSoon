e5363ae14c5501644257d68a203845e44138b76a
geekymoose <constantin.masson@umontreal.ca>
2018-03-25 12:32:26 -0400

    Divide PlayerMovement into 2 scripts: movement and pickup

--- Assets/Scripts/Player/PlayerAction.cs
+++ Assets/Scripts/Player/PlayerAction.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class PlayerAction : MonoBehaviour {
+    private bool canPickup = false;
+    private Transform objectToPickUp;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +14,53 @@ public class PlayerAction : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //the player have to be in the collider of the pickup object if he want to pick up the object
+        if (Input.GetButtonDown("Fire1") && canPickup)
+        {
+            Debug.Log("PlayerMovement::PICKUP");
+            objectToPickUp.transform.parent = transform;
+            objectToPickUp.gameObject.GetComponent<Rigidbody2D>().simulated = false;
+        }
+
+        //when the player release the button drop the object
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            Debug.Log("PlayerMovement::DROP");
+            canPickup = false;
+            this.Drop();
+        }
 	}
+	
+    //drop the object when the player release the button
+    public void Drop()
+    {
+        if(this.objectToPickUp != null) {
+            Debug.Log("PlayerMovement::Drop()");
+            objectToPickUp.parent = null;
+            //objectToPickUp = GameObject.FindGameObjectWithTag("Pickup").transform;
+            objectToPickUp.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            objectToPickUp.gameObject.GetComponent<Rigidbody2D>().simulated = true;
+        }
+    }
+
+    //if the player collide on the object that he can pickup
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Pickup")
+        {
+            this.objectToPickUp = other.gameObject.transform;
+            canPickup = true;
+        }
+        else if(other.CompareTag("Goal")) {
+            RoomGoal roger = other.gameObject.GetComponent<RoomGoal>();
+            Assert.IsNotNull(roger, "Goal object doesn't have a RoomGoal script");
+            roger.activate();
+        }
+    }
+
+    //deactivate the pickup when the player exit the collider
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canPickup = false;
+    }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using UnityEngine.Assertions;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -7,10 +6,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
 
     private Rigidbody2D body2d;
-    private bool canPickup = false;
-    private Transform objectToPickUp;
     private Vector2 movementVector;
-    private Vector2 forward;
     private Animator anim;
 
     // Use this for initialization
@@ -23,21 +19,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     public void Update()
     {
-        //the player have to be in the collider of the pickup object if he want to pick up the object
-        if (Input.GetButtonDown("Fire1") && canPickup)
-        {
-            Debug.Log("PlayerMovement::PICKUP");
-            objectToPickUp.transform.parent = transform;
-            objectToPickUp.gameObject.GetComponent<Rigidbody2D>().simulated = false;
-        }
-
-        //when the player release the button drop the object
-        else if (Input.GetButtonUp("Fire1"))
-        {
-            Debug.Log("PlayerMovement::DROP");
-            canPickup = false;
-            this.Drop();
-        }
         //movement with the axis of the xbox gamepad
         movementVector.x = Input.GetAxisRaw("Horizontal") * speed;
         movementVector.y = Input.GetAxisRaw("Vertical") * speed;
@@ -46,7 +27,6 @@ public class PlayerMovement : MonoBehaviour
         {
             anim.SetBool("canMove", true);
             DefinePlayerDirection(movementVector);
-            
         }
         else
         {
@@ -59,43 +39,10 @@ public class PlayerMovement : MonoBehaviour
         body2d.velocity = new Vector2(movementVector.x, movementVector.y);
     }
 
-    //drop the object when the player release the button
-    public void Drop()
-    {
-        if(this.objectToPickUp != null) {
-            Debug.Log("PlayerMovement::Drop()");
-            objectToPickUp.parent = null;
-            //objectToPickUp = GameObject.FindGameObjectWithTag("Pickup").transform;
-            objectToPickUp.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-            objectToPickUp.gameObject.GetComponent<Rigidbody2D>().simulated = true;
-        }
-    }
-
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.green;
         Gizmos.DrawRay(transform.position, movementVector.normalized);
-    }
-
-    //if the player collide on the object that he can pickup
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Pickup")
-        {
-            this.objectToPickUp = other.gameObject.transform;
-            canPickup = true;
-        }
-        else if(other.CompareTag("Goal")) {
-            RoomGoal roger = other.gameObject.GetComponent<RoomGoal>();
-            Assert.IsNotNull(roger, "Goal object doesn't have a RoomGoal script");
-            roger.activate();
-        }
-    }
-
-    //deactivate the pickup when the player exit the collider
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        canPickup = false;
     }
 
     public void DefinePlayerDirection(Vector2 move)
