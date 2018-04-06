0cad233a0b25f3fd6de2c0be54b372585b077290
geekymoose <constantin.masson@umontreal.ca>
2018-04-05 22:33:54 -0400

    Create welcome menu

---
+++ Assets/Scripts/UI/MenuWelcome.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuWelcome : MonoBehaviour {
+	
+	public void startGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void quit() {
+		Application.Quit();
+	}
+}
